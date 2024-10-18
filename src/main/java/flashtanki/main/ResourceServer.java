ec0878cece62b9a7cd51eca6eabe4c8d0329aca7
Bodya <Vavrencukbogdan28@gmail.com>
2024-10-18 23:49:09 +0300

    d

--- src/main/java/flashtanki/main/ResourceServer.java
+++ src/main/java/flashtanki/main/ResourceServer.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.nio.file.Files;
 
 public class ResourceServer {
     static {
@@ -30,23 +29,29 @@ public class ResourceServer {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             String pathStr = "configurations" + exchange.getRequestURI().getPath();
-            System.out.println(pathStr);
+            LoggerService.log(LogType.INFO, "Starting to download resource: " + pathStr);
 
-            InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(pathStr);
-            if (resourceStream != null) {
-                byte[] response = resourceStream.readAllBytes();
-                exchange.sendResponseHeaders(200, response.length);
-                OutputStream os = exchange.getResponseBody();
-                os.write(response);
-                os.close();
-                LoggerService.log(LogType.INFO, "The localization file (" + pathStr + ") was successfully sent");
-            } else {
-                String response = "404 (Not Found)";
-                exchange.sendResponseHeaders(404, response.length());
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-                LoggerService.log(LogType.INFO, "The localization file (" + pathStr + ") not found");
+            try (InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(pathStr)) {
+                if (resourceStream != null) {
+                    byte[] response = resourceStream.readAllBytes();
+                    exchange.sendResponseHeaders(200, response.length);
+                    LoggerService.log(LogType.INFO, "Resource found: " + pathStr + " | Size: " + response.length + " bytes");
+
+                    try (OutputStream os = exchange.getResponseBody()) {
+                        os.write(response);
+                        LoggerService.log(LogType.INFO, "Successfully sent resource: " + pathStr);
+                    }
+                } else {
+                    String response = "404 (Not Found)";
+                    exchange.sendResponseHeaders(404, response.length());
+                    try (OutputStream os = exchange.getResponseBody()) {
+                        os.write(response.getBytes());
+                    }
+                    LoggerService.log(LogType.WARNING, "Resource not found: " + pathStr);
+                }
+            } catch (IOException e) {
+                LoggerService.log(LogType.ERROR, "Error while handling resource: " + pathStr + " | Exception: " + e.getMessage());
+                throw e;
             }
         }
     }
