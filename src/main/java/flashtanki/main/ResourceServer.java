15744945147efed24b904014b92d4f8d0a0e86c4
Bodya <Vavrencukbogdan28@gmail.com>
2024-10-24 22:00:06 +0300

    add

--- src/main/java/flashtanki/main/ResourceServer.java
+++ src/main/java/flashtanki/main/ResourceServer.java
@@ -8,50 +8,40 @@ import flashtanki.logger.remote.types.LogType;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import java.nio.file.Files;
 
 public class ResourceServer {
-    static {
-        LoggerService.getInstance();
-    }
 
     public static void start() throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/localization/", new LocalizationHandler());
+        HttpServer server = HttpServer.create(new java.net.InetSocketAddress(8080), 0);
+        server.createContext("/resources/", new LocalizationHandler());
         server.setExecutor(null);
         server.start();
-        LoggerService.log(LogType.INFO, "Resource server started");
+        LoggerService.log(LogType.INFO,"Resource server started");
     }
 
     static class LocalizationHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             String pathStr = "configurations" + exchange.getRequestURI().getPath();
-            LoggerService.log(LogType.INFO, "Starting to download resource: " + pathStr);
+            System.out.println(pathStr);
+            File file = new File(pathStr);
 
-            try (InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(pathStr)) {
-                if (resourceStream != null) {
-                    byte[] response = resourceStream.readAllBytes();
-                    exchange.sendResponseHeaders(200, response.length);
-                    LoggerService.log(LogType.INFO, "Resource found: " + pathStr + " | Size: " + response.length + " bytes");
-
-                    try (OutputStream os = exchange.getResponseBody()) {
-                        os.write(response);
-                        LoggerService.log(LogType.INFO, "Successfully sent resource: " + pathStr);
-                    }
-                } else {
-                    String response = "404 (Not Found)";
-                    exchange.sendResponseHeaders(404, response.length());
-                    try (OutputStream os = exchange.getResponseBody()) {
-                        os.write(response.getBytes());
-                    }
-                    LoggerService.log(LogType.WARNING, "Resource not found: " + pathStr);
-                }
-            } catch (IOException e) {
-                LoggerService.log(LogType.ERROR, "Error while handling resource: " + pathStr + " | Exception: " + e.getMessage());
-                throw e;
+            if (file.exists()) {
+                byte[] response = Files.readAllBytes(file.toPath());
+                exchange.sendResponseHeaders(200, response.length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(response);
+                os.close();
+                LoggerService.log(LogType.INFO,"The localization file (" + file.getPath() + ") was successfully sent");
+            } else {
+                String response = "404 (Not Found)";
+                exchange.sendResponseHeaders(404, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+                LoggerService.log(LogType.INFO,"The localization file (" + file.getPath() + ") not found");
             }
         }
     }
